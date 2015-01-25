343000f7cc1d4b4538c1064b8e7c00f4c101c659
Debarshi Chaudhuri <debarshi.chaudhuri@gmail.com>
2015-01-25 13:21:50 -0800

    Did some shit and now layout works again.

--- sharebear/settings.py
+++ sharebear/settings.py
@@ -89,6 +89,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'allauth.account.context_processors.account',
     'allauth.socialaccount.context_processors.socialaccount',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
     )
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
